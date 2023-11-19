8d39b1f55afc99e55542814731adc4266f60762f
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2023-11-19 23:12:27 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,15 +1,11 @@
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-import java.time.temporal.WeekFields
-import java.util.Locale
+// Assuming you have a list of monthly amounts (replace this with your data)
+val monthlyAmounts: List[Double] = List(100.0, 150.0, 200.0, 180.0, 210.0, 190.0, 220.0, 240.0, 260.0, 280.0, 300.0, 320.0)
 
-// Set a specific date (January 1st)
-val specificDate = LocalDate.of(2023, 1, 1)
+// Get the current month (replace with your logic to get the current month)
+val currentMonth: Int = java.time.LocalDate.now().getMonthValue
 
-// Extract week number using ISO definition
-val weekNumberISO = specificDate.get(WeekFields.ISO.weekOfWeekBasedYear())
+// Calculate YTD amount
+val ytdAmount: Double = monthlyAmounts.take(currentMonth).sum
 
-// Format week number with leading zeros (e.g., "01", "02", ..., "52")
-val formattedWeekNumber = f"$weekNumberISO%02d"
-
-println(s"Formatted Week number (ISO) for January 1st: $formattedWeekNumber")
+// Display YTD amount
+println(s"Year-to-Date (YTD) amount: $ytdAmount")
