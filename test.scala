d9a1e6bb1b082d90575a53cf886edae9593ce1fe
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2023-11-19 21:34:23 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -2,23 +2,19 @@ import java.time.LocalDate
 import java.time.temporal.WeekFields
 import java.util.Locale
 
-object DateExtractor {
-  def main(args: Array[String]): Unit = {
-    val currentDate = LocalDate.now()
+val currentDate = LocalDate.now()
 
-    // Extract current month
-    val currentMonth = currentDate.getMonthValue
-    println(s"Current month: $currentMonth")
+// Extract current month
+val currentMonth = currentDate.getMonthValue
+print(s"Current month: $currentMonth\n")
 
-    // Extract week number using ISO definition
-    val weekNumberISO = currentDate.get(WeekFields.ISO.weekOfWeekBasedYear())
-    println(s"Week number (ISO): $weekNumberISO")
+// Extract week number using ISO definition
+val weekNumberISO = currentDate.get(WeekFields.ISO.weekOfWeekBasedYear())
+print(s"Week number (ISO): $weekNumberISO\n")
 
-    // Define a Locale for Zurich, Switzerland (German-speaking part)
-    val zurichLocale = new Locale("de", "CH") // Language: German, Country: Switzerland
+// Define a Locale for Zurich, Switzerland (German-speaking part)
+val zurichLocale = new Locale("de", "CH") // Language: German, Country: Switzerland
 
-    // Extract week number using custom definition (e.g., Zurich Locale)
-    val weekNumberZurich = currentDate.get(WeekFields.of(zurichLocale).weekOfWeekBasedYear())
-    println(s"Week number (Custom, Zurich Locale): $weekNumberZurich")
-  }
-}
+// Extract week number using custom definition (e.g., Zurich Locale)
+val weekNumberZurich = currentDate.get(WeekFields.of(zurichLocale).weekOfWeekBasedYear())
+print(s"Week number (Custom, Zurich Locale): $weekNumberZurich\n")
