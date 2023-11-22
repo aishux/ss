a9b3c40e90115fc506e4cc2790467fa552aa8972
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2023-11-22 12:18:36 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,18 +1,30 @@
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 
-// Assuming `timeDf` is a DataFrame
+// Create a function to perform the required operations
+def calculateSumAmount(dataFrame: DataFrame): Double = {
+  val filteredDF = dataFrame
+    .filter(col("EXT_MSR_IDENT") === "CA123")
+    .filter(col("EXT_TIM_DETAIL").like("%2023%"))
+    .filter(col("EXT_TVT_IDENT") === "M")
+    .filter(col("QQ") < 4)
 
-// Calculate the last date of the current week
-val currentDate = java.time.LocalDate.now()
-val currentDayOfWeek = currentDate.getDayOfWeek.getValue
-val daysUntilEndOfWeek = java.time.DayOfWeek.SUNDAY.getValue - currentDayOfWeek
-val lastDateOfCurrentWeek = currentDate.plusDays(daysUntilEndOfWeek)
-val formattedDate = lastDateOfCurrentWeek.format(java.time.format.DateTimeFormatter.ofPattern("yyyyMMdd"))
+  // Calculate the sum of the AMOUNT_USD column for the filtered rows
+  val sumAmount = filteredDF.agg(sum("AMOUNT_USD")).collect()(0).getDouble(0)
 
-// Filtering based on conditions
-val filteredDF = timeDf
-  .filter(col("QQ") === 4)
-  .filter(to_date(col("ext_tim_ident"), "yyyyMMdd").cast("string") <= formattedDate)
+  sumAmount
+}
 
-// Displaying the filtered DataFrame
-filteredDF.show()
+// Assuming 'spark' is your SparkSession and 'yourDataFrame' is your DataFrame
+val spark = SparkSession.builder()
+  .appName("CalculateSumAmountExample")
+  .getOrCreate()
+
+// Assuming 'yourDataFrame' is the DataFrame you mentioned
+val yourDataFrame = ???
+
+// Calculate the sum of AMOUNT_USD column based on the specified criteria
+val sumOfAmount = calculateSumAmount(yourDataFrame)
+
+// Display the calculated sum
+println(s"The sum of AMOUNT_USD for the filtered rows is: $sumOfAmount")
