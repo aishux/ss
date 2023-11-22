3d3be8b49dccbbcb0dc2d099b81e43eb0a7a4147
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2023-11-22 21:57:46 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,28 +1,21 @@
-import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 
-// Assuming 'spark' is your SparkSession
-val spark = SparkSession.builder()
-  .appName("JoinAndReplaceExample")
-  .getOrCreate()
-
-// Assuming 'timeDf' and 'resultsDF' are your DataFrames
-val timeDf: DataFrame = ??? // Replace ??? with your timeDf DataFrame
+// Assuming 'timeDetailDf' and 'resultsDF' are your DataFrames
+val timeDetailDf: DataFrame = ??? // Replace ??? with your timeDetailDf DataFrame
 val resultsDF: DataFrame = ??? // Replace ??? with your resultsDF DataFrame
 
-// Join timeDf and resultsDF based on EXT_MSR_IDENT and EXT_TIM_IDENT columns
-val joinedDF = timeDf.join(resultsDF,
-  timeDf("EXT_MSR_IDENT") === resultsDF("current_EXT_MSR_IDENT") &&
-  timeDf("EXT_TIM_IDENT") === resultsDF("current_EXT_TIM_IDENT"),
+val joinedDF = timeDetailDf.join(
+  resultsDF,
+  timeDetailDf("EXT_MSR_IDENT") === resultsDF("current_EXT_MSR_IDENT") &&
+    timeDetailDf("EXT_TIM_IDENT") === resultsDF("current_EXT_TIM_IDENT"),
   "left_outer"
 )
 
-// Replace Amount_USD, Amount_CHF, Amount_GBP columns in timeDf with columns from resultsDF
-val updatedTimeDf = joinedDF
-  .withColumn("Amount_USD", coalesce(col("AmountUSD"), col("Amount_USD")))
-  .withColumn("Amount_CHF", coalesce(col("AmountCHF"), col("Amount_CHF")))
-  .withColumn("Amount_GBP", coalesce(col("AmountGBP"), col("Amount_GBP")))
-  .drop("AmountUSD", "AmountCHF", "AmountGBP")
+val updatedTimeDetailDf = joinedDF
+  .withColumn("YTD_USD", when(resultsDF("AmountUSD").isNotNull, resultsDF("AmountUSD")).otherwise(timeDetailDf("YTD_USD")))
+  .withColumn("YTD_EUR", when(resultsDF("AmountEUR").isNotNull, resultsDF("AmountEUR")).otherwise(timeDetailDf("YTD_EUR")))
+  .withColumn("YTD_GBP", when(resultsDF("AmountGBP").isNotNull, resultsDF("AmountGBP")).otherwise(timeDetailDf("YTD_GBP")))
+  .withColumn("YTD_CHF", when(resultsDF("AmountCHF").isNotNull, resultsDF("AmountCHF")).otherwise(timeDetailDf("YTD_CHF")))
+  .drop("AmountUSD", "AmountEUR", "AmountGBP", "AmountCHF")
 
-// Show the updated timeDf DataFrame
-updatedTimeDf.show()
+updatedTimeDetailDf.show()
