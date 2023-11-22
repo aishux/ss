2a09e74a30d8a7fe504b942ea3f90d3814620fbd
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2023-11-22 22:15:34 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,21 +1,21 @@
 import org.apache.spark.sql.functions._
 
-// Assuming 'timeDetailDf' and 'resultsDF' are your DataFrames
 val timeDetailDf: DataFrame = ??? // Replace ??? with your timeDetailDf DataFrame
 val resultsDF: DataFrame = ??? // Replace ??? with your resultsDF DataFrame
 
 val joinedDF = timeDetailDf.join(
-  resultsDF,
+  resultsDF
+    .withColumn("formatted_EXT_TIM_IDENT", date_format(col("current_EXT_TIM_IDENT"), "yyyyMMdd")),
   timeDetailDf("EXT_MSR_IDENT") === resultsDF("current_EXT_MSR_IDENT") &&
-    timeDetailDf("EXT_TIM_IDENT") === resultsDF("current_EXT_TIM_IDENT"),
+    timeDetailDf("EXT_TIM_IDENT") === col("formatted_EXT_TIM_IDENT"),
   "left_outer"
 )
 
 val updatedTimeDetailDf = joinedDF
-  .withColumn("YTD_USD", when(resultsDF("AmountUSD").isNotNull, resultsDF("AmountUSD")).otherwise(timeDetailDf("YTD_USD")))
-  .withColumn("YTD_EUR", when(resultsDF("AmountEUR").isNotNull, resultsDF("AmountEUR")).otherwise(timeDetailDf("YTD_EUR")))
-  .withColumn("YTD_GBP", when(resultsDF("AmountGBP").isNotNull, resultsDF("AmountGBP")).otherwise(timeDetailDf("YTD_GBP")))
-  .withColumn("YTD_CHF", when(resultsDF("AmountCHF").isNotNull, resultsDF("AmountCHF")).otherwise(timeDetailDf("YTD_CHF")))
-  .drop("AmountUSD", "AmountEUR", "AmountGBP", "AmountCHF")
+  .withColumn("YTD_USD", coalesce(resultsDF("AmountUSD"), timeDetailDf("YTD_USD")))
+  .withColumn("YTD_EUR", coalesce(resultsDF("AmountEUR"), timeDetailDf("YTD_EUR")))
+  .withColumn("YTD_GBP", coalesce(resultsDF("AmountGBP"), timeDetailDf("YTD_GBP")))
+  .withColumn("YTD_CHF", coalesce(resultsDF("AmountCHF"), timeDetailDf("YTD_CHF")))
+  .drop("AmountUSD", "AmountEUR", "AmountGBP", "AmountCHF", "formatted_EXT_TIM_IDENT")
 
 updatedTimeDetailDf.show()
