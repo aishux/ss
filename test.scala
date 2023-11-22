7e4b2e76194e4c433e24d71f68088079fd669d4e
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2023-11-22 09:58:31 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,11 +1,11 @@
-// Assuming you have a list of monthly amounts (replace this with your data)
-val monthlyAmounts: List[Double] = List(100.0, 150.0, 200.0, 180.0, 210.0, 190.0, 220.0, 240.0, 260.0, 280.0, 300.0, 320.0)
+import org.apache.spark.sql.functions._
 
-// Get the current month (replace with your logic to get the current month)
-val currentMonth: Int = java.time.LocalDate.now().getMonthValue
+// Assuming `timeDf` is a DataFrame
 
-// Calculate YTD amount
-val ytdAmount: Double = monthlyAmounts.take(currentMonth).sum + monthlyAmounts.drop(currentMonth).sum
+// Filtering based on conditions
+val filteredDF = timeDf
+  .filter(col("QQ") === 4)
+  .filter(col("ext_tim_ident").isin(timeDf.select(max("ext_tim_ident")).collect(): _*))
 
-// Display YTD amount
-println(s"Year-to-Date (YTD) amount: $ytdAmount")
+// Displaying the filtered DataFrame
+filteredDF.show()
