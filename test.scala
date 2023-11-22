f900f722b996e53c5f58856b2ac948b8e8b30ca7
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2023-11-22 10:23:12 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -5,7 +5,7 @@ import org.apache.spark.sql.functions._
 // Filtering based on conditions
 val filteredDF = timeDf
   .filter(col("QQ") === 4)
-  .filter(col("ext_tim_ident").isin(timeDf.select(max("ext_tim_ident")).collect(): _*))
+  .filter(to_date(col("ext_tim_ident"), "yyyyMMdd").cast("string") <= "2023-11-24")
 
 // Displaying the filtered DataFrame
 filteredDF.show()
