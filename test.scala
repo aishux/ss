d1f81b446a605ef0ea5d837b4006151c8894a8e0
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2023-11-26 00:25:38 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -9,5 +9,10 @@ val spark = SparkSession.builder().appName("DataFrame Merge").getOrCreate()
 val commonColumns = df1.columns.toSet.intersect(df2.columns.toSet).map(col).toSeq
 val mergedDf: DataFrame = df1.select(commonColumns: _*).union(df2.select(commonColumns: _*)).distinct()
 
-// Show the resulting DataFrame
-mergedDf.show()
+// Order the resulting DataFrame by a specific column or list of columns
+val orderByColumns = Seq("A", "B") // Replace with the column(s) you want to order by
+val orderedMergedDf = mergedDf.orderBy(orderByColumns.map(col): _*)
+
+// Show the resulting ordered DataFrame
+orderedMergedDf.show()
+
