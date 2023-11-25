efb6b33ce3bfeac9e92db6341d5c6860d18e4678
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2023-11-25 12:16:52 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,30 +1,35 @@
-import org.apache.spark.sql.{SparkSession, functions}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.expressions.Window
 
 // Create a SparkSession
 val spark = SparkSession.builder()
-  .appName("Calculate Total USD")
+  .appName("Calculate Cumulative Sum")
   .master("local[*]")
   .getOrCreate()
 
-// Sample data
-val data = Seq(
-  ("ABC", 20230131, "M", 10),
-  // ... (your complete dataset)
-  ("XYZ", 20231231, "M", 20)
-)
-
 import spark.implicits._
 
-// Create a DataFrame from the provided data
+// Sample DataFrame
+val data = Seq(
+  ("ABC", "20230131", "M", 10),
+  ("ABC", "20230228", "M", 10),
+  // Add other rows here
+  ("XYZ", "20231130", "M", 20),
+  ("XYZ", "20231231", "M", 20)
+)
+
 val df = data.toDF("EXT_MSR_IDENT", "EXT_TIM_IDENT", "EXT_TVT_IDENT", "AMOUNT_USD")
-  .withColumn("EXT_TIM_IDENT", $"EXT_TIM_IDENT".cast("int"))
 
-// Calculate the total USD for each EXT_MSR_IDENT up to the current month
-val windowSpec = Window.partitionBy("EXT_MSR_IDENT").orderBy("EXT_TIM_IDENT").rowsBetween(Window.unboundedPreceding, Window.currentRow)
+// Convert 'EXT_TIM_IDENT' column to date type
+val dateFormat = "yyyyMMdd"
+val dfFormatted = df.withColumn("EXT_TIM_IDENT", to_date($"EXT_TIM_IDENT", dateFormat))
 
-val result = df
-  .withColumn("TOTAL_USD_CURR_MONTH", functions.sum($"AMOUNT_USD").over(windowSpec))
-  .orderBy("EXT_MSR_IDENT", "EXT_TIM_IDENT")
+// Define a Window specification partitioned by 'EXT_MSR_IDENT' and ordered by 'EXT_TIM_IDENT'
+val windowSpec = Window.partitionBy("EXT_MSR_IDENT").orderBy("EXT_TIM_IDENT")
+
+// Calculate cumulative sum using window functions
+val result = dfFormatted
+  .withColumn("TOTAL_USD_CURR_MONTH", sum("AMOUNT_USD").over(windowSpec))
 
 result.show()
