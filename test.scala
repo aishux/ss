20dc9e61e198d9e0d6e6b7526b3e4d322ea8ed4a
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2023-11-27 19:37:46 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,16 +1,14 @@
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.DataFrame
 
-// Assuming 'inputDF' is your DataFrame containing the provided data
+// Assuming 'mergedDf' is your DataFrame
 
-val quarter = 4
-val QQ_MINUS_1 = 3
+val variable_name = "20233010" // Your variable containing the date string
 
-// Calculate the maximum date when QQ_MINUS_1 matches EXT_TVT_IDENT using when and withColumn
-val resultDF = inputDF.withColumn(
-  "max_date",
-  when(col("QQ") === QQ_MINUS_1 && col("EXT_TVT_IDENT") === quarter, max("EXT_TIM_IDENT").over())
-    .otherwise(null) // Set to null if conditions are not met
+val updatedDf = mergedDf.withColumn(
+  "YTD_USD",
+  when(col("EXT_TVT_IDENT") === "M" && col("EXT_TIM_IDENT") === variable_name,
+    col("FINAL_AMOUNT_USD")
+  ).otherwise(col("YTD_USD"))
 )
 
-resultDF.show() // Display the DataFrame with the new 'max_date' column
+updatedDf.show() // Show the updated DataFrame
