4a5507a4d5d422facdf4780549846a6369365720
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2023-11-27 23:34:39 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,18 +1,15 @@
-import org.apache.spark.sql.functions._
 import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions._
 
-// Assuming 'mergedDf' is your DataFrame and 'week_date_variable' and 'month_date_variable' are your variables
+// Assuming 'mergedDf' is your DataFrame
 
-val windowSpec = Window.partitionBy("ID")
+val windowSpec = Window.partitionBy("EXT_MSR_IDENT").orderBy("EXT_TIM_IDENT")
 
-val updatedDf = mergedDf.withColumn("QTD_USD",
-  when(col("EXT_TVT_IDENT") === "M" && col("EXT_TIM_IDENT") === month_date_variable,
-    coalesce(
-      first(when(col("EXT_TVT_IDENT") === "W" && col("EXT_TIM_IDENT") === week_date_variable, col("WEEKLY_TOTAL_AMOUNT_USD")))
-        .over(windowSpec),
-      col("QTD_USD")
-    )
+val updatedDf = mergedDf.withColumn(
+  "QTD_USD",
+  when(col("EXT_TIM_IDENT") === "20231130",
+    last(when(col("EXT_TIM_IDENT") === "20231124", col("YTD_USD")), ignoreNulls = true).over(windowSpec)
   ).otherwise(col("QTD_USD"))
 )
 
-updatedDf.where(col("EXT_MSR_IDENT") === "123").show()
+updatedDf.show()
