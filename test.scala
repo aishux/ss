94ba13d2b8901ac321c94e42ec400c41cdb3cff7
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2023-11-27 20:29:17 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,14 +1,15 @@
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.expressions.Window
 
-// Assuming 'mergedDf' is your DataFrame
+// Assuming 'mergedDf' is your DataFrame and 'week_date_variable' and 'month_date_variable' are your variables
 
-val variable_name = "20233010" // Your variable containing the date string
+val windowSpec = Window.partitionBy("ID")
 
-val updatedDf = mergedDf.withColumn(
-  "YTD_USD",
-  when(col("EXT_TVT_IDENT") === "M" && col("EXT_TIM_IDENT") === variable_name,
-    col("FINAL_AMOUNT_USD")
-  ).otherwise(col("YTD_USD"))
+val updatedDf = mergedDf.withColumn("QTD_USD",
+  when(col("EXT_TVT_IDENT") === "M" && col("EXT_TIM_IDENT") === month_date_variable,
+    first(when(col("EXT_TVT_IDENT") === "W" && col("EXT_TIM_IDENT") === week_date_variable, col("WEEKLY_TOTAL_AMOUNT_USD")))
+      .over(windowSpec)
+  ).otherwise(col("QTD_USD"))
 )
 
 updatedDf.show() // Show the updated DataFrame
