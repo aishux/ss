457c432f34f52babc746f80a709a89ec24e2a610
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2023-12-01 10:51:55 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,22 +1,23 @@
 import re
 
-# Sample words with alphabets and numbers
-words = ["1abc2", "3xyzsh7skks", "8jkdkd"]
-
 # Function to extract numbers and calculate their sum
-def extract_and_sum(numbers):
+def extract_and_sum(words):
     total_sum = 0
-    for word in numbers:
+    for word in words:
         extracted_numbers = re.findall(r'\d+', word)  # Extract numbers using regex
         if len(extracted_numbers) == 1:
-            num = int(extracted_numbers[0])
-            total_sum += num * 2  # Consider one number twice
+            num = int(extracted_numbers[0] * 2)  # Consider one number twice
+            total_sum += num
         elif len(extracted_numbers) >= 2:
-            num1 = int(extracted_numbers[0])
-            num2 = int(extracted_numbers[1])
-            total_sum += num1 + num2
+            concatenated_numbers = ''.join(extracted_numbers)  # Concatenate multiple numbers
+            total_sum += int(concatenated_numbers)
 
     return total_sum
+
+# Read words from a file where each word is on a separate line
+file_path = 'path_to_your_file.txt'  # Replace with the actual file path
+with open(file_path, 'r') as file:
+    words = file.read().splitlines()
 
 result = extract_and_sum(words)
 print("The sum of the extracted numbers:", result)
