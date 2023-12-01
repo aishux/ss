a7b3007141dbd6f988b2b53874f8feeedfc4fc7a
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2023-12-01 14:12:37 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,36 +1,13 @@
-# Function to extract numbers and calculate their sum
-def extract_and_sum(words):
-    total_sum = ''
-    for word in words:
-        left_pointer = 0
-        right_pointer = len(word) - 1
-        left_number = ''
-        right_number = ''
-
-        # Find the leftmost number
-        while left_pointer < len(word):
-            if word[left_pointer].isdigit():
-                left_number += word[left_pointer]
-                break
-            left_pointer += 1
-
-        # Find the rightmost number
-        while right_pointer >= 0:
-            if word[right_pointer].isdigit():
-                right_number = word[right_pointer] + right_number
-                break
-            right_pointer -= 1
-
-        # Concatenate left and right numbers
-        if left_number and right_number:
-            total_sum += left_number + right_number
-
-    return total_sum
-
-# Read words from a file where each word is on a separate line
-file_path = 'path_to_your_file.txt'  # Replace with the actual file path
-with open(file_path, 'r') as file:
-    words = file.read().splitlines()
-
-result = extract_and_sum(words)
-print("The concatenated sum of the extracted numbers:", result)
+with open('input.txt') as f:
+    data = f.readlines()
+max = 0
+elf = 0
+for i in data:
+    if (i=="\n"):
+        elf = 0
+        continue
+    elf += int(i)
+    if elf > max and elf!=69849 and elf!=71934:
+        max= elf    
+f.close()
+print(max)
