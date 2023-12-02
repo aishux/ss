c6ed5fb84521f6e7cc9b64bbb63fc572a50c1f1c
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2023-12-02 10:40:53 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,13 +1,23 @@
-with open('input.txt') as f:
-    data = f.readlines()
-max = 0
-elf = 0
-for i in data:
-    if (i=="\n"):
-        elf = 0
-        continue
-    elf += int(i)
-    if elf > max and elf!=69849 and elf!=71934:
-        max= elf    
-f.close()
-print(max)
+input_data = [
+    "Game 1: 3 blue, 4 red; 2 blue 3 green; 5 red, 7 blue, 8 green",
+    "Game 2: 2 blue, 4 red; 1 blue 2 green"
+]
+
+game_data = {}
+
+for game in input_data:
+    game_info = game.split(': ')
+    game_name = game_info[0]
+    colors_data = game_info[1].split('; ')
+
+    game_data[game_name] = {}
+    for colors in colors_data:
+        color_list = colors.split(', ')
+        for color in color_list:
+            quantity, color_name = color.split(' ')
+            if color_name not in game_data[game_name]:
+                game_data[game_name][color_name] = int(quantity)
+            else:
+                game_data[game_name][color_name] += int(quantity)
+
+print(game_data)
