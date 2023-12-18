cf31a6c87d3be342d006b1b05870c840889e4a84
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2023-12-18 15:15:27 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,16 +1,29 @@
-import java.time.{DayOfWeek, LocalDate}
+import org.apache.spark.sql.{SparkSession, DataFrame}
+import org.apache.spark.sql.functions._
 
-// Get today's date
-val today = LocalDate.now()
+// Assuming you already have a SparkSession created
+val spark = SparkSession.builder().appName("UpdateValuesExample").getOrCreate()
 
-// Find the day of the week (0: Saturday, 1: Sunday, ..., 6: Friday)
-val dayOfWeek = today.getDayOfWeek().getValue()
+// Assuming 'df' is your DataFrame
+val df: DataFrame = // ... your DataFrame creation or retrieval
 
-// Calculate the difference to get to the end of the week (Friday: 6)
-val daysToAdd = (6 - dayOfWeek + DayOfWeek.SATURDAY.getValue()) % 7
+// Define a function to update values when CHILD_CODE is 'CA-GEBO'
+def updateValues(childCode: String, parentCode: String, derivedPK: String): (String, String) => String = {
+  (cc: String, pc: String) =>
+    if (cc == childCode) {
+      derivedPK // New value for DERIVED_PK when CHILD_CODE is 'CA-GEBO'
+    } else {
+      pc // Keep the existing value for PARENT_CODE
+    }
+}
 
-// Calculate the end date of the current week (Friday)
-val weekEndDate = today.plusDays(daysToAdd)
+// Define the values to update
+val newParentCode = "new_parent_value"
+val newDerivedPK = "new_derived_pk_value"
 
-// Display the end date of the current week (Friday)
-println("End date of the current week (Friday): " + weekEndDate)
+// Update the values based on the condition CHILD_CODE = 'CA-GEBO'
+val updatedDF = df.withColumn("PARENT_CODE", when(col("CHILD_CODE") === "CA-GEBO", lit(newParentCode)).otherwise(col("PARENT_CODE")))
+  .withColumn("DERIVED_PK", when(col("CHILD_CODE") === "CA-GEBO", lit(newDerivedPK)).otherwise(col("DERIVED_PK")))
+
+// Show the updated DataFrame
+updatedDF.show()
