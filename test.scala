9ed9de3b76505d2b4cf88de9560a8b09e525f517
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-02-05 23:53:21 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,22 +1,17 @@
-import java.time.{LocalDate, DayOfWeek, WeekFields, DateTimeFormatter}
+import java.time.{LocalDate, DayOfWeek, WeekFields, TemporalAdjusters, DateTimeFormatter}
 import java.util.Locale
 
-def generateWeeklyData(year: Int, hierarchy_type: String): Seq[(String, String, String, String, String, String, String, String, String, String, String)] = {
-  val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
-  val firstDayOfYear = LocalDate.of(year, 1, 1) // Updated to start from January 1st
-  val weekFields = WeekFields.of(Locale.getDefault())
+def generateWeeklyData(year: Int, hierarchy_type: String) = {
+    val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
+    val firstFridayOfYear = LocalDate.of(year, 1, 1)
+      .with(TemporalAdjusters.nextOrSame(DayOfWeek.FRIDAY))
+    val weekFields = WeekFields.of(DayOfWeek.MONDAY, 1)
 
-  (1 to firstDayOfYear.range(weekFields.weekOfWeekBasedYear()).getMaximum.toInt).map { week =>
-    val weekStartDate = firstDayOfYear.`with`(weekFields.weekOfWeekBasedYear(), week.toLong)
-    val weekEndDate = weekStartDate.`with`(DayOfWeek.FRIDAY)
-    val weekNumber = f"$week%02d"
-    val weekDesc = s"$year $weekNumber"
+    Iterator.iterate(firstFridayOfYear)(.plusDays(7)).takeWhile(.getYear == year).map { weekStartDate =>
+      val weekEndDate = weekStartDate.with(DayOfWeek.FRIDAY)
+      val weekNumber = weekStartDate.get(weekFields.weekOfWeekBasedYear)
+      val weekDesc = s"$year ${f"$weekNumber%02d"}"
 
-    ("WEEKLY", "Weekly Periods", "0", s"W_$year", s"$year", weekEndDate.format(formatter), weekDesc, "0", "W", hierarchy_type)
+      ("WEEKLY", "Weekly Periods", "0", s"W_$year", s"$year", weekEndDate.format(formatter), weekDesc, "0", "W", hierarchy_type)
+    }
   }
-}
-
-val lastYear = LocalDate.now().getYear - 1
-
-val result = generateWeeklyData(lastYear, "kyvos_gwm_pc")
-result.foreach(println)
