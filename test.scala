565aa99484b21802315bccecffe0efee48472506
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-02-13 20:30:55 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,12 +1,7 @@
 import org.apache.spark.sql.functions.{col, expr}
 
 // Assuming df is your DataFrame
-val dfWithIntMonth = df.withColumn("month_number_int", col("month_number").cast("int"))
+val dfWithTempA = df.withColumn("month_number_int", col("month_number").cast("int"))
+                     .withColumn("temp_a", expr("annual_ytd / 12 * (3 + month_number_int)"))
 
-// Remove leading zeros from the integer month column
-val dfCleanedMonth = dfWithIntMonth.withColumn("month_number_int", expr("lpad(month_number_int, 2, '0')"))
-
-// If you prefer using regexp_replace:
-// val dfCleanedMonth = dfWithIntMonth.withColumn("month_number_int", expr("regexp_replace(month_number_int, '^0+', '')"))
-
-dfCleanedMonth.show()
+dfWithTempA.show()
