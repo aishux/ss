12d1f2b3509f6027b0a4e7e75ab9abf7b80d9589
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-03-05 20:52:37 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,7 +1,2 @@
-import org.apache.spark.sql.functions.{col, expr}
-
-// Assuming df is your DataFrame
-val dfWithTempA = df.withColumn("month_number_int", col("month_number").cast("int"))
-                     .withColumn("temp_a", expr("annual_ytd / 12 * (3 + month_number_int)"))
-
-dfWithTempA.show()
+SELECT DATE_FORMAT(STR_TO_DATE(date_column, '%Y%m%d'), '%Y%m %b') AS formatted_date
+FROM your_table;
