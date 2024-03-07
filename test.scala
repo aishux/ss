f4b66bd30e814bf3f1c52e09f2966622ad643073
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-03-07 16:24:49 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,5 +1,6 @@
-SELECT concat_ws(';', collect_set(email)) AS VU1_EMAILS
-FROM (
-    SELECT explode(split(VU1_EMAILS, ';')) AS email
-    FROM your_table
-) AS emails;
+-- Update the original table with unique values
+UPDATE test
+SET A = (
+    SELECT CONCAT_WS(';', COLLECT_SET(UniqueValue))
+    FROM TempUniqueValues
+);
