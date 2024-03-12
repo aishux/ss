2e28218678706251981d7261302e1e350808c6f9
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-03-12 23:19:16 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,1 +1,15 @@
-col("t1.level_code") === concat_ws("", col("CA"), col("t2.access_object"))
+val resultDF = your_table_name
+  .groupBy(
+    col("ACCESS_OBJECT"),
+    col("LEVEL1"),
+    col("LEVEL2"),
+    col("LEVEL3"),
+    col("SYSTEM_CODE")
+  )
+  .agg(
+    max(when(col("SYSTEM_CODE") === "VU1", concat_ws(";", collect_set("EMAIL")))).alias("VU1_EMAILS"),
+    max(when(col("SYSTEM_CODE") === "VW3", concat_ws(";", collect_set("EMAIL")))).alias("VW3_EMAILS")
+  )
+
+// Show the result
+resultDF.show()
