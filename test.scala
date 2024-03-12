f4c5bbbe02baf75e2e7c4f58a1c7e82bb3430e6e
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-03-12 22:45:57 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,7 +1,1 @@
-val joinConditions = orgCols.map(orgCol =>
-      col("b.access_object") === orgCol ||
-        expr(s"CONCAT('CA-', b.access_object)") === orgCol
-    )
-
-    // Dynamically construct the join condition
-    val joinCondition = joinConditions.reduce(_ || _)
+val joinCondition = (1 to 20).map(i => (org_table(s"level$i") === bbs_table("access_object")) || (org_table(s"level$i") === ("CA-" + bbs_table("access_object")))).reduce(_ || _)
