ba69243477ec4b30853319bc39895d0bcd8a388c
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-03-12 23:30:06 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,15 +1,6 @@
-val resultDF = your_table_name
-  .groupBy(
-    col("ACCESS_OBJECT"),
-    col("LEVEL1"),
-    col("LEVEL2"),
-    col("LEVEL3"),
-    col("SYSTEM_CODE")
-  )
-  .agg(
-    max(when(col("SYSTEM_CODE") === "VU1", concat_ws(";", collect_set("EMAIL")))).alias("VU1_EMAILS"),
-    max(when(col("SYSTEM_CODE") === "VW3", concat_ws(";", collect_set("EMAIL")))).alias("VW3_EMAILS")
-  )
+// Define group by columns dynamically
+val groupByColumns = (1 to 20).map(i => col(s"level$i"))
 
-// Show the result
-resultDF.show()
+// Perform group by and aggregation
+val groupedResult = finalResult.groupBy(groupByColumns: _*)
+                              .agg(collect_set("emails").as("VU1"))
