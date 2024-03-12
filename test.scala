306e49e1e1185864dc378f8500e17887df15c7e8
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-03-12 23:04:03 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,1 +1,1 @@
-val joinCondition = (1 to 20).map(i => (org_table(s"level$i") === bbs_table("access_object")) || (org_table(s"level$i") === ("CA-" + bbs_table("access_object")))).reduce(_ || _)
+col("t1.level_code") === concat_ws("", col("CA"), col("t2.access_object"))
