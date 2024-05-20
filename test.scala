d33db8e1ffd33daca918168f1d729d8d7fb55fcd
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-05-21 01:09:57 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,52 +1,21 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
-// Create SparkSession
-val spark = SparkSession.builder()
-  .appName("AddColumnExample")
-  .getOrCreate()
+val spark = SparkSession.builder.appName("DefaultColumn").getOrCreate()
+import spark.implicits._
 
-// Sample data
+// Sample DataFrame
 val data = Seq(
-  ("USD", "CHF", 1.158),
-  ("USD", "EUR", 1.08),
-  ("USD", "GBP", 1.266),
-  ("USD", "USD", 1.00)
-)
+  (1, "oldA"),
+  (2, "oldA")
+).toDF("id", "A")
 
-// Create DataFrame
-val df = spark.createDataFrame(data).toDF("FROM", "TO", "EX_RT")
+// Perform transformation on column "A" and add column "B" if it doesn't exist
+val columns = data.columns
 
-// Filter rows where TO column is 'CHF'
-val chfValue = df.filter(col("TO") === "CHF").select("EX_RT").collect()(0)(0)
+val resultDF = data
+  .withColumn("A", lit("A"))
+  .withColumn("B", when(lit(columns.contains("B")), col("B")).otherwise(lit("XYZ")))
 
-// Add new column with constant value
-val resultDF = df.withColumn("EX_RT_CHF", lit(chfValue))
-
-// Show result
+// Display the result
 resultDF.show()
-
-
-#######
-
-import org.apache.spark.sql.functions._
-
-// Group by ID and get the CHF value for each group
-val chfValues = df.groupBy("ID").agg(first(when(col("TO") === "CHF", col("EX_RT"))).alias("EX_RT_CHF"))
-
-// Join the original DataFrame with the calculated CHF values
-val resultDF = df.join(chfValues, Seq("ID"), "left")
-
-// Show result
-resultDF.show()
-
-#######
-
-SELECT t1.A, t1.B, t1.C, t1.D, t1.E, t1.F, t1.G, t2.F AS J
-FROM Temp1 AS t1
-LEFT JOIN (
-    SELECT A, B, C, G, F
-    FROM Temp1
-    WHERE D = 'CHF' AND E = 'USD'
-) AS t2
-ON t1.A = t2.A AND t1.B = t2.B AND t1.C = t2.C AND t1.G = t2.G;
