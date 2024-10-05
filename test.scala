eb3a866137d83221d55646d3a63ed47803012bab
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-10-06 01:24:02 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,8 +1,7 @@
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.SparkSession
-import java.time.LocalDate
+import java.time.{LocalDate, YearMonth}
 import java.time.format.DateTimeFormatter
-import org.apache.spark.sql.expressions.Window
 
 // Create a SparkSession
 val spark = SparkSession.builder.appName("DateExplode").getOrCreate()
@@ -14,22 +13,25 @@ val data = Seq(
   ("20040131", "20061231", 1)
 ).toDF("VALID_FROM", "VALID_TO", "ID")
 
-// UDF to generate a sequence of months between two dates
-val generateMonths = udf((startDateStr: String, endDateStr: String) => {
+// UDF to generate a sequence of months with the last day between two dates
+val generateMonthsLastDay = udf((startDateStr: String, endDateStr: String) => {
   val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
   val startDate = LocalDate.parse(startDateStr, formatter).withDayOfMonth(1)
   val endDate = LocalDate.parse(endDateStr, formatter).withDayOfMonth(1)
 
-  // Generate a sequence of months between the two dates
+  // Generate a sequence of months between the two dates, but using the last day of each month
   Iterator.iterate(startDate)(_.plusMonths(1))
     .takeWhile(!_.isAfter(endDate))
-    .map(_.format(formatter))
+    .map(date => {
+      val yearMonth = YearMonth.of(date.getYear, date.getMonthValue)
+      yearMonth.atEndOfMonth().format(formatter) // Get last day of each month
+    })
     .toSeq
 })
 
 // Apply the UDF and explode the sequence
 val result = data
-  .withColumn("Month", explode(generateMonths(col("VALID_FROM"), col("VALID_TO"))))
+  .withColumn("Month", explode(generateMonthsLastDay(col("VALID_FROM"), col("VALID_TO"))))
   .select("ID", "Month")
 
 // Show the result
