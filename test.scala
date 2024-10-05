ba0bb16e7e53101db2b494527967db8a8ac81150
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2024-10-06 01:21:48 +0530

    Update test.scala

--- test.scala
+++ test.scala
@@ -1,31 +1,36 @@
 import org.apache.spark.sql.functions._
-import java.time.{LocalDate, YearMonth}
-import java.time.format.DateTimeFormatter
 import org.apache.spark.sql.SparkSession
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import org.apache.spark.sql.expressions.Window
 
 // Create a SparkSession
-val spark = SparkSession.builder.appName("DateConversion").getOrCreate()
-
+val spark = SparkSession.builder.appName("DateExplode").getOrCreate()
 import spark.implicits._
 
 // Sample data
 val data = Seq(
-  ("20030401"),
-  ("20040101")
-).toDF("VALID_FROM")
+  ("20030430", "20031130", 1),
+  ("20040131", "20061231", 1)
+).toDF("VALID_FROM", "VALID_TO", "ID")
 
-// UDF to convert date to last day of month
-val convertToLastDay = udf((dateStr: String) => {
+// UDF to generate a sequence of months between two dates
+val generateMonths = udf((startDateStr: String, endDateStr: String) => {
   val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
-  val parsedDate = LocalDate.parse(dateStr, formatter)
-  val year = parsedDate.getYear
-  val month = parsedDate.getMonthValue
-  val lastDayOfMonth = YearMonth.of(year, month).atEndOfMonth()
-  lastDayOfMonth.format(formatter)
+  val startDate = LocalDate.parse(startDateStr, formatter).withDayOfMonth(1)
+  val endDate = LocalDate.parse(endDateStr, formatter).withDayOfMonth(1)
+
+  // Generate a sequence of months between the two dates
+  Iterator.iterate(startDate)(_.plusMonths(1))
+    .takeWhile(!_.isAfter(endDate))
+    .map(_.format(formatter))
+    .toSeq
 })
 
-// Apply the UDF to the DataFrame
-val result = data.withColumn("VALID_FROM", convertToLastDay(col("VALID_FROM")))
+// Apply the UDF and explode the sequence
+val result = data
+  .withColumn("Month", explode(generateMonths(col("VALID_FROM"), col("VALID_TO"))))
+  .select("ID", "Month")
 
 // Show the result
 result.show(false)
