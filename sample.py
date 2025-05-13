882a8a048d0ab7c03b063957b584ecca4ba0dd74
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2025-05-13 19:34:04 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,32 +1,34 @@
-red_balls = 12
-green_balls = 13
-blue_balls = 14
+import os
+import yaml
+import json
 
-data = {
-    'Game 1': ['4 blue, 5 red', '6 green, 7 red, 8 blue'],
-    'Game 2': ['2 blue, 5 red', '6 green, 0 red, 8 blue', '8 green, 7 blue, 2 red'],
-    'Game 3': ['1 blue, 2 blue', '6 green, 7 green, 8 red']
-}
+class SummationAgent:
+    def __init__(self, env_name, business_division, ...):  # Add other existing params
+        self.env_name = env_name
+        self.business_division = business_division.lower()  # e.g., "gf"
+        
+        self.load_config_and_prompts()
 
-keys_satisfying_criteria = []
+        # Use these loaded configs
+        self.table_name = self.config["commentary_summation"]["table_name"]
+        self.columns = self.config["commentary_summation"]["columns"]
+        self.output_table = self.config["commentary_summation"]["output_table"]
+        self.prompt_template = self.prompts.get(f"{business_division.upper()}_summation_prompt")
 
-for key, sets in data.items():
-    red_total = 0
-    green_total = 0
-    blue_total = 0
+        ...
+        # (rest of init logic continues)
 
-    for game_set in sets:
-        elements = game_set.split(', ')
-        for element in elements:
-            quantity, color = element.split(' ')
-            if color == 'red':
-                red_total += int(quantity)
-            elif color == 'green':
-                green_total += int(quantity)
-            elif color == 'blue':
-                blue_total += int(quantity)
+    def load_config_and_prompts(self):
+        # Path to the env + business division folder
+        base_path = os.path.join(self.env_name, self.business_division)
 
-    if red_total < red_balls and green_total < green_balls and blue_total < blue_balls:
-        keys_satisfying_criteria.append(key)
+        config_path = os.path.join(base_path, "config.yaml")
+        prompts_path = os.path.join(base_path, "prompts.json")
 
-print("Keys satisfying the criteria:", keys_satisfying_criteria)
+        # Load YAML config
+        with open(config_path, "r") as f:
+            self.config = yaml.safe_load(f)
+
+        # Load JSON prompts
+        with open(prompts_path, "r") as f:
+            self.prompts = json.load(f)
