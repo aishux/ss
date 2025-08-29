c8d7d169570366095486a115185d92b96075893b
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2025-08-29 16:04:29 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,5 @@
-import re
-from PyPDF2 import PdfReader
+from azure.cli.core import get_default_cli
 
-reader = PdfReader("template.pdf")
-response_template = ""
-for page in reader.pages:
-    text = page.extract_text()
+az_cli = get_default_cli()
 
-    # Fix case where sentences get glued (period followed by space + capital letter/word)
-    text = re.sub(r"(?<=\.)\s+([A-Z])", r"\n\n\1", text)
-
-    # Normalize multiple newlines (preserve original paragraph spacing)
-    text = re.sub(r"\n{3,}", "\n\n", text)
-
-    response_template += text.strip() + "\n\n"  # ensure separation between pages
-
-print(response_template)
+az_cli.invoke(['login', '--service-principal', '-u', '<appId>', '-p', 'password','--tenant','teanat id'])
