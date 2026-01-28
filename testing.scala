55d33ab7d9d9aab6d85e5a7acadb5224e12f5796
Nikhil Mankani <55614782+niksm7@users.noreply.github.com>
2026-01-29 00:47:12 +0530

    Update testing.scala

--- testing.scala
+++ testing.scala
@@ -5,3 +5,11 @@
 Then("""^the output should be "([^"]*)", "([^"]*)", "([^"]*)"$""") { (v1: String, v2: String, v3: String) =>
   outputRows.headOption.map(_.toSeq.map(_.toString)) shouldBe Some(Seq(v1, v2, v3))
 }
+
+
+Then("""^the output should be "([^"]*)", "([^"]*)", "([^"]*)"$""") { (v1: String, v2: String, v3: String) =>
+  val actualOpt = Option(outputRows).flatMap(_.headOption).map { row =>
+    row.toSeq.map(e => java.lang.String.valueOf(e))
+  }
+  actualOpt shouldBe Some(Seq(v1, v2, v3))
+}
