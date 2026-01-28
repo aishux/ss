bf3695ee51a8afc73f92c655355b1e3eb95987b9
Aishwarya Nathani <61228436+aishux@users.noreply.github.com>
2026-01-28 20:28:20 +0530

    Update testing.scala

--- testing.scala
+++ testing.scala
@@ -1,4 +1,7 @@
-// Note the added commas between the {string} placeholders
-Then("""the output should be "{string}", "{string}", "{string}" """) { (v1: String, v2: String, v3: String) =>
+// 1. We use Regex syntax: ([^"]*)
+// 2. We explicitly match the comma AND the space
+// 3. I corrected 'headoption' to 'headOption' (Scala is case sensitive)
+
+Then("""^the output should be "([^"]*)", "([^"]*)", "([^"]*)"$""") { (v1: String, v2: String, v3: String) =>
   outputRows.headOption.map(_.toSeq.map(_.toString)) shouldBe Some(Seq(v1, v2, v3))
 }
